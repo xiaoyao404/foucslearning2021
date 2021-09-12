4d8956ed0e91a03b73c5ea0377f22453ac15ad1c
miller <441997573@qq.com>
2021-09-12 14:26:42 +0800

    update 01vuex

---
+++ 01-vuex/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h2>{{message}}</h2>
+      <h1>{{counter}}</h1>
+      <button @click="counter++">+</button>
+      <button @click="counter--" :disabled="counter<=0">-</button>
+    <helllo-vuex :counter2="counter"></helllo-vuex>
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+import HellloVuex from './components/HellloVuex.vue'
+
+export default {
+  components: { HellloVuex },
+  name: 'App',
+  data(){
+    return{
+      counter:0,
+      message:'我是App组件'
+    }
+  }
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ 01-vuex/src/components/HellloVuex.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+
+
+<h2>{{message2}}</h2>
+<h2>{{counter2}}</h2>
+</div>
+
+    
+</template>
+
+
+<script>
+
+export default {
+    name:'HelloVuex',
+    props:{
+        counter2:Number
+    },
+    data(){
+        return{
+            message2:'子组件',
+            
+        }
+    }
+
+
+}
+</script>
+
+<style >
+
+</style>
